56e052bf38152cd3eba33285c5bd92347bef16f2
John Coggeshall <john@coggeshall.org>
2018-06-08 14:29:22 -0400

    Update CoogleEEPROM.cpp

--- src/CoogleEEPROM.cpp
+++ src/CoogleEEPROM.cpp
@@ -2,7 +2,7 @@
   +----------------------------------------------------------------------+
   | CoogleIOT for ESP8266                                                |
   +----------------------------------------------------------------------+
-  | Copyright (c) 2017 John Coggeshall                                   |
+  | Copyright (c) 2017-2018 John Coggeshall                              |
   +----------------------------------------------------------------------+
   | Licensed under the Apache License, Version 2.0 (the "License");      |
   | you may not use this file except in compliance with the License. You |
